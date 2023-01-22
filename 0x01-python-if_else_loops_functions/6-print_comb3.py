6f5042366c8330d471bec84268c8fe9ad303759d
Opaldot <oomoleye@ymail.com>
2023-01-22 18:10:48 +0000

    all tasks

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+n range(10):
+    for y in range(10):
+        if x == 8 and y == 9:
+            print('{}{}'.format(x, y))
+        elif x != y and x < y:
+            print('{}{}, '.format(x, y), end='')
